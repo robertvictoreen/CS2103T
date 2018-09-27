89ed362747427f80720dfd4ce7cf0902010c0cda
Zachary Tan <zackkh95@gmail.com>
2018-09-27 01:08:18 -0700

    Add SortCommand Class

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Sorts all persons in the address book lexicographically.
+ */
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_SUCCESS = "Sorted all persons in the Address Book";
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        model.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
