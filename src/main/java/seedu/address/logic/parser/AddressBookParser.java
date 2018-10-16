68f1036a3a5d74a8d643f266901c81ca46fe4ce7
zzsmao <42551763+zzsmao@users.noreply.github.com>
2018-10-17 00:50:06 +0800

    Zhen Hao's changes ** ATTENDANCE PREFIX CHANGED TO /at **

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.AddAssignmentCommand;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddProfilePictureCommand;
+import seedu.address.logic.commands.AttendanceCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteAssignmentCommand;
@@ -60,6 +61,9 @@ public class AddressBookParser {
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
 
+        case AttendanceCommand.COMMAND_WORD:
+                return new AttendanceCommandParser().parse(arguments);
+            
         case AddProfilePictureCommand.COMMAND_WORD:
             return new AddPictureCommandParser().parse(arguments);
 
