75e1ad55a8ea239bb3dcd6b40b6c4601e2670eb4
spencertan96 <spencertan_1220@hotmail.com>
2018-10-18 16:04:31 +0800

    Fix checkstyle Add comments

--- src/main/java/seedu/address/model/person/AssignmentStub.java
+++ src/main/java/seedu/address/model/person/AssignmentStub.java
@@ -1,11 +1,14 @@
 package seedu.address.model.person;
 
+/**
+ * Stub class to represent each of a student's assignments.
+ */
 public class AssignmentStub {
     //private totalNumber;
     private String name;
     private float marks;
 
-    public AssignmentStub(String title, float mark){
+    public AssignmentStub(String title, float mark) {
         name = title;
         marks = mark;
     }
--- src/main/java/seedu/address/ui/MoreDetailsPanel.java
+++ src/main/java/seedu/address/ui/MoreDetailsPanel.java
@@ -1,28 +1,19 @@
 package seedu.address.ui;
 
 import java.lang.reflect.Array;
-import java.net.URL;
 import java.util.logging.Logger;
 
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Font;
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.AssignmentStub;
-import seedu.address.model.person.Person;
 
 /**
  * The More Details Panel of the App.
@@ -41,7 +32,7 @@ public class MoreDetailsPanel extends UiPart<Region> {
 
     // Initializing test data
     private AssignmentStub[] assignments = {new AssignmentStub("Finals Exam", 73), new AssignmentStub("Mid-terms", 39),
-            new AssignmentStub("Participation", 7), new AssignmentStub("Assignment 1", 22)};
+        new AssignmentStub("Participation", 7), new AssignmentStub("Assignment 1", 22)};
 
     @FXML
     private TextArea notesText;
@@ -54,10 +45,10 @@ public class MoreDetailsPanel extends UiPart<Region> {
         super(FXML);
 
         // if student has no recorded assignments
-        if (false) {// student no assignments){
+        if (false) { // student no assignments){
             // default label
             Label noComponents = new Label("<No assignments entered>");
-            noComponents.setFont(new Font("System", (double)25));
+            noComponents.setFont(new Font("System", (double) 25));
             components.add(noComponents, 0, 0);
         } else {
             // add 2 columns, default has 1
@@ -68,14 +59,14 @@ public class MoreDetailsPanel extends UiPart<Region> {
             components.getRowConstraints().add(newRow);
 
             // add no. of rows equal to no. of assignments keyed in
-            for (int i = 0; i < Array.getLength(assignments); i++){
+            for (int i = 0; i < Array.getLength(assignments); i++) {
                 Label toAdd = new Label(assignments[i].getName());
-                toAdd.setStyle("-fx-font-size: 11pt;\n" + "-fx-font-family: \"Segoe UI Semibold\";\n" +
-                        "-fx-text-fill: white;\n" + "-fx-opacity: 1;");
+                toAdd.setStyle("-fx-font-size: 11pt;\n" + "-fx-font-family: \"Segoe UI Semibold\";\n"
+                    + "-fx-text-fill: white;\n" + "-fx-opacity: 1;");
                 components.add(toAdd, 0, i);
                 Label marksLabel = new Label(Float.toString(assignments[i].getMarks()));
-                marksLabel.setStyle("-fx-font-size: 11pt;\n" + "-fx-font-family: \"Segoe UI Semibold\";\n" +
-                        "-fx-text-fill: white;\n" + "-fx-opacity: 1;");
+                marksLabel.setStyle("-fx-font-size: 11pt;\n" + "-fx-font-family: \"Segoe UI Semibold\";\n"
+                    + "-fx-text-fill: white;\n" + "-fx-opacity: 1;");
                 components.add(marksLabel, 1, i);
             }
         }
