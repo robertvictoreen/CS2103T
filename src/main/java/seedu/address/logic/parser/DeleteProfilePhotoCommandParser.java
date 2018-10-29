1e557ac5e33f12919316ca0fd5ff3afacd8860f4
Zachary Tan <zackkh95@gmail.com>
2018-10-29 03:29:59 -0700

    Update UG and improve formatting

--- src/main/java/seedu/address/logic/parser/DeleteProfilePhotoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteProfilePhotoCommandParser.java
@@ -17,8 +17,8 @@ public class DeleteProfilePhotoCommandParser implements Parser<DeleteProfilePhot
     private String path;
     private Index index;
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteByNameCommand
-     * and returns an DeleteByNameCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteProfilePhotoCommand
+     * and returns an DeleteProfilePhotoCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteProfilePhotoCommand parse(String args) throws ParseException {
