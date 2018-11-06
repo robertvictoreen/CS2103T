343760c7abcf74f4a281fe75896cf6e39ead8a48
HenryJk <hsmgj2010@gmail.com>
2018-11-06 21:30:16 +0800

    Fix style

--- src/test/java/seedu/address/logic/commands/DeleteProfilePhotoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteProfilePhotoCommandTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
