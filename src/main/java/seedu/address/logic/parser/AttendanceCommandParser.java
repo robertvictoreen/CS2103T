ba781e08dd5a3512c42ed45bce7b62cd3d26858e
robertvictoreen <robert.victoreen@u.nus.edu>
2018-11-12 23:45:22 +0800

    Validator

--- src/main/java/seedu/address/logic/parser/AttendanceCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AttendanceCommandParser.java
@@ -1,23 +1,15 @@
 package seedu.address.logic.parser;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MARK;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AttendanceCommand;
 
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.assignment.Mark;
-import seedu.address.model.attendance.Attendance;
 import seedu.address.model.attendance.AttendanceMark;
 
 /**
